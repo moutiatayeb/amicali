ecc73bd350b68d6715041d58b91ecc2db34cb15d
moutiatayeb <tayebmoutia@gmail.com>
2020-04-19 23:59:11 +0200

    Initial commit

---
+++ src/main/java/com/pfa/amicali/AmicaliApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.amicali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmicaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmicaliApplication.class, args);
+	}
+
+}
